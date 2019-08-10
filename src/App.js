d8068ea77c99c18191b286797c5b7ff7f45e1491
Csaba Schreiner <schreiner.csaba@gmail.com>
2019-08-10 23:52:21 +0200

    feat: read from json, additional casters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
+import Spells from './spells';
 
-var color = {
+const color = {
     bard: 'red',
-    wizard: 'purple',
-    cleric: 'blue'
+    cleric: 'blue',
+    druid: 'lightgreen',
+    paladin: 'white',
+    ranger: 'darkgreen',
+    sorcerer: 'orange',
+    warlock: 'black',
+    wizard: 'purple'
 }
+
+const casterTypes = ['Bard', 'Cleric', 'Druid', 'Paladin', 'Ranger', 'Sorcerer', 'Warlock', 'Wizard'];
+
+for (var currentKey in Spells) {
+    console.log(Spells[currentKey]);
+    Spells[currentKey]['casters'] = [];
+    Spells[currentKey]['additionalCasters'] = [];
+    for (var casterType of casterTypes) {
+        if (Spells[currentKey]['caster' + casterType] === casterType) {
+            Spells[currentKey]['casters'].push(casterType.toLowerCase());
+        } else if (Spells[currentKey]['caster' + casterType]) {
+            Spells[currentKey]['additionalCasters'].push(casterType.toLowerCase());
+        }
+    }
+}
+
+console.log(Spells);
 
 function Gradientize(casters) {
     let gradient = 'background: conic-gradient(';
@@ -23,15 +46,53 @@ function Gradientize(casters) {
 
 const Board = styled.div`
     display: grid;
-    grid-template-columns: repeat(20, 1fr);
-    grid-column-gap: 0.5vw;
-    grid-row-gap: 0.5vw;
-    margin: 0.5vw;
+    grid-template-columns: repeat(30, 1fr);
+    grid-column-gap: 0.75vw;
+    grid-row-gap: 0.75vw;
+    margin: 0.75vw;
+    max-height: 100%;
+`;
+
+function AdditionalCaster(props) {
+
+    return (
+        <div className={props.className}></div>
+    );
+}
+
+const StyledAdditionalCaster = styled(AdditionalCaster)`
+    position: absolute;
+    width: 0.75vw;
+    height: 0.75vw;
+    border-radius: 50%;
+    background-color: ${props => color[props.caster]};
+
+    :first-of-type {
+        top: 0;
+        left: 0;
+    }
+
+    :nth-of-type(2) {
+        top: 0;
+        right: 0;
+    }
 `;
 
 function Spell(props) {
+
+    function renderAdditionalCasters(props) {
+        if (props.additionalCasters) {
+
+            return props.additionalCasters.map(additionalCaster => <StyledAdditionalCaster caster={additionalCaster}/>);
+        }
+
+        else return '';
+    }
+
     return (
-        <div className={props.className}></div>
+        <div className={props.className}>
+            {renderAdditionalCasters(props)}
+        </div>
     );
 }
 
@@ -43,6 +104,7 @@ const StyledSpell = styled(Spell)`
     ${props => props.coordinates ? 'grid-column-start: ' + props.coordinates[0] + ';' : ''}
     ${props => props.coordinates ? 'grid-row-start: ' + props.coordinates[1] + ';' : ''}
     box-shadow: 0 0 2vw 0 #000;
+    position: relative;
 `;
 
 class App extends React.Component {
@@ -51,6 +113,7 @@ class App extends React.Component {
         spells: [
             {
                 'casters': ['bard', 'wizard', 'cleric'],
+                'additionalCasters': ['wizard', 'cleric'],
                 'coordinates': [3, 1]
             },
             {
@@ -62,7 +125,8 @@ class App extends React.Component {
                 'coordinates': [9, 1]
             },
             {
-                'casters': ['bard', 'wizard', 'cleric'],
+                'casters': ['bard'],
+                'additionalCasters': ['cleric'],
                 'coordinates': [18, 2]
             },
             {
@@ -73,7 +137,15 @@ class App extends React.Component {
     };
 
     renderSpells() {
-        return this.state.spells.map((spell) => <StyledSpell casters={spell.casters} coordinates={spell.coordinates} />);
+        var spellList = [];
+        for (var currentKey in Spells) {
+            //console.log(Spells[currentKey]['additionalCasters']);
+            spellList.push(<StyledSpell casters={Spells[currentKey]['casters']} additionalCasters={Spells[currentKey]['additionalCasters']} />);
+        }
+
+        return spellList;
+
+        //return this.state.spells.map((spell) => <StyledSpell casters={spell.casters} additionalCasters={spell.additionalCasters} coordinates={spell.coordinates} />);
     }
 
     render() {
