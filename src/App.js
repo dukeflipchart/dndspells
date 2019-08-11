b91f6668480547c6ba62a327290683b65d01eb6c
Csaba Schreiner <schreiner.csaba@gmail.com>
2019-08-11 21:24:31 +0200

    feat: improved positions

--- src/App.js
+++ src/App.js
@@ -13,21 +13,23 @@ const color = {
     wizard: '#4C88FF'
 }
 
-const casterTypes = ['Bard', 'Ranger', 'Druid', 'Cleric', 'Paladin', 'Warlock', 'Sorcerer', 'Wizard'];
+/* Parsing a Google Sheets JSON */
 
-//var coordX = 1;
-//var coordY = 1;
+/*const casterTypes = ['Bard', 'Ranger', 'Druid', 'Cleric', 'Paladin', 'Warlock', 'Sorcerer', 'Wizard'];
+
+var coordX = 1;
+var coordY = 1;
 
 for (var currentKey in Spells) {
 
     Spells[currentKey]['id'] = currentKey;
     Spells[currentKey]['casters'] = [];
     Spells[currentKey]['additionalCasters'] = [];
-    /*Spells[currentKey]['coordinates'] = [coordX, coordY];
+    Spells[currentKey]['coordinates'] = [coordX, coordY];
     if (++coordX > 19) {
         coordX = 1;
         coordY++;
-    }*/
+    }
 
     for (var casterType of casterTypes) {
         if (Spells[currentKey]['caster' + casterType] === casterType) {
@@ -38,7 +40,7 @@ for (var currentKey in Spells) {
     }
 
     //console.log(Spells[currentKey]);
-}
+}*/
 
 function Gradientize(casters) {
     let gradient = 'background: conic-gradient(';
@@ -59,7 +61,11 @@ const Board = styled.div`
     grid-template-columns: repeat(19, 1fr);
     grid-gap: 0.75vw;
     margin: 0.75vw;
-    max-height: 100%;
+    @media only screen and (min-width: 1280px) {
+        width: calc(1280px - 1.5 * 12.8px);
+        grid-gap: calc(0.75 * 12.8px);
+        margin: calc(0.75 * 12.8px) auto;
+    }
 `;
 
 function AdditionalCaster(props) {
@@ -85,6 +91,11 @@ const StyledAdditionalCaster = styled(AdditionalCaster)`
         top: 0;
         right: 0;
     }
+
+    @media only screen and (min-width: 1280px) {
+        width: 12.8px;
+        height: 12.8px;
+    }
 `;
 
 function Spell(props) {
@@ -101,7 +112,7 @@ function Spell(props) {
     return (
         <div className={props.className} onClick={() => props.onClick()}>
             {renderAdditionalCasters(props)}
-            <SpellLevel>{props.id}</SpellLevel>
+            <SpellLevel></SpellLevel>
         </div>
     );
 }
@@ -132,7 +143,18 @@ const SpellLevel = styled.div`
     font-size: 1.5vw;
     line-height: 1.5vw;
     text-align: center;
+
+    @media only screen and (min-width: 1280px) {
+        top: calc(50% - 0.75 * 12.8px);
+        font-size: calc(1.5 * 12.8px);
+        line-height: calc(1.5 * 12.8px);
+    }
 `;
+
+function SaveButton(props) {
+
+    return <button onClick={() => props.onClick()}>Save</button>;
+}
 
 class App extends React.Component {
 
@@ -141,38 +163,80 @@ class App extends React.Component {
 
         this.state = {
             'spells': [],
-            'selectedId': false
+            'selectedId': false,
+            'timeSinceLastSave': 0
         };
 
-        for (var currentKey in Spells) {
+        var currentKey=0;
+        for (currentKey in Spells) {
             this.state.spells[currentKey] = Spells[currentKey];
-            //console.log(this.state.spells[currentKey]);
+            console.log(this.state.spells[currentKey]);
         }
 
         this.state.selectedId = false;
     }
 
-    renderSpell(spellId) {
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            'timeSinceLastSave': this.state['timeSinceLastSave'] + 1000
+        });
+        if (this.state['timeSinceLastSave'] > 300000) {
+            this.handleSave(this.state.spells);
+            this.setState({
+                'timeSinceLastSave': 0
+            });
+            console.log('Saved');
+        }
+    }
+
+    handleSave(jsonData) {
+        const fileData = JSON.stringify(jsonData);
+        const blob = new Blob([fileData], {type: "text/plain"});
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.download = 'filename.json';
+        link.href = url;
+        link.click();
+    }
+
+    renderSpell(i) {
         //console.log(this.state.spells[spellId]['name']);
 
         return (<StyledSpell
-            id={spellId}
-            casters={this.state.spells[spellId]['casters']}
-            additionalCasters={this.state.spells[spellId]['additionalCasters']}
-            level={this.state.spells[spellId]['level']}
-            selected={this.state.selectedId === spellId ? true : false}
-            coordinates={this.state.spells[spellId]['coordinates']}
-            onClick={() => this.handleClick(spellId)}
+            id={this.state.spells[i]['id']}
+            casters={this.state.spells[i]['casters']}
+            additionalCasters={this.state.spells[i]['additionalCasters']}
+            level={this.state.spells[i]['level']}
+            selected={this.state.selectedId === i ? true : false}
+            coordinates={this.state.spells[i]['coordinates']}
+            onClick={() => this.handleClick(i)}
         />);
     }
 
-    handleClick(spellId) {
-        if (this.state.selectedId === spellId) {
+    handleClick(i) {
+        if (this.state.selectedId === i) {
             this.setState({'selectedId': false});
-            console.log('unselected: '+spellId);
+            console.log('unselected: '+i);
+        } else if (this.state.selectedId) {
+            console.log('switching position of ' + this.state.selectedId + ' and ' + i);
+            var newCoords = this.state.spells[i]['coordinates'];
+            this.state.spells[i]['coordinates'] = this.state.spells[this.state.selectedId]['coordinates'];
+            this.state.spells[this.state.selectedId]['coordinates'] = newCoords;
+            this.setState({'selectedId': false});
         } else {
-            this.setState({'selectedId': spellId});
-            console.log('selected: '+spellId);
+            this.setState({'selectedId': i});
+            console.log('selected: ' + i);
         }
     }
 
@@ -189,9 +253,12 @@ class App extends React.Component {
 
     render() {
         return (
-            <Board>
-                {this.renderSpells()}
-            </Board>
+            <>
+                <Board>
+                    {this.renderSpells()}
+                </Board>
+                <SaveButton onClick={() => this.handleSave(this.state.spells)} />
+            </>
         );
     }
 }
