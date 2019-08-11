3af46d0e8623440efa52531424b3362676e54bce
Csaba Schreiner <schreiner.csaba@gmail.com>
2019-08-11 10:41:28 +0200

    feat: add id, level, new colors

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import styled from 'styled-components';
 import Spells from './spells';
 
 const color = {
-    bard: 'red',
-    cleric: 'blue',
-    druid: 'lightgreen',
-    paladin: 'white',
-    ranger: 'darkgreen',
-    sorcerer: 'orange',
-    warlock: 'black',
-    wizard: 'purple'
+    bard: '#E052E0',
+    cleric: '#EB4747',
+    druid: '#60DF20',
+    paladin: '#F5D63D',
+    ranger: '#2EB82E',
+    sorcerer: '#F2800D',
+    warlock: '#A852FF',
+    wizard: '#4C88FF'
 }
 
 const casterTypes = ['Bard', 'Cleric', 'Druid', 'Paladin', 'Ranger', 'Sorcerer', 'Warlock', 'Wizard'];
 
 for (var currentKey in Spells) {
     console.log(Spells[currentKey]);
+    Spells[currentKey]['id'] = currentKey;
     Spells[currentKey]['casters'] = [];
     Spells[currentKey]['additionalCasters'] = [];
     for (var casterType of casterTypes) {
@@ -47,8 +48,7 @@ function Gradientize(casters) {
 const Board = styled.div`
     display: grid;
     grid-template-columns: repeat(19, 1fr);
-    grid-column-gap: 0.75vw;
-    grid-row-gap: 0.75vw;
+    grid-gap: 0.75vw;
     margin: 0.75vw;
     max-height: 100%;
 `;
@@ -62,8 +62,8 @@ function AdditionalCaster(props) {
 
 const StyledAdditionalCaster = styled(AdditionalCaster)`
     position: absolute;
-    width: 0.75vw;
-    height: 0.75vw;
+    width: 1vw;
+    height: 1vw;
     border-radius: 50%;
     background-color: ${props => color[props.caster]};
 
@@ -92,6 +92,7 @@ function Spell(props) {
     return (
         <div className={props.className}>
             {renderAdditionalCasters(props)}
+            <SpellLevel>{props.level}</SpellLevel>
         </div>
     );
 }
@@ -107,45 +108,33 @@ const StyledSpell = styled(Spell)`
     position: relative;
 `;
 
+const SpellLevel = styled.div`
+    position: absolute;
+    top: calc(50% - 0.75vw);
+    left: 0;
+    right: 0;
+    font-size: 1.5vw;
+    line-height: 1.5vw;
+    text-align: center;
+`;
+
 class App extends React.Component {
 
     state = {
-        spells: [
-            {
-                'casters': ['bard', 'wizard', 'cleric'],
-                'additionalCasters': ['wizard', 'cleric'],
-                'coordinates': [3, 1]
-            },
-            {
-                'casters': ['bard', 'wizard'],
-                'coordinates': [5, 1]
-            },
-            {
-                'casters': ['wizard', 'cleric'],
-                'coordinates': [9, 1]
-            },
-            {
-                'casters': ['bard'],
-                'additionalCasters': ['cleric'],
-                'coordinates': [18, 2]
-            },
-            {
-                'casters': ['bard', 'wizard', 'cleric'],
-                'coordinates': [3, 3]
-            }
-        ]
     };
 
     renderSpells() {
         var spellList = [];
         for (var currentKey in Spells) {
             //console.log(Spells[currentKey]['additionalCasters']);
-            spellList.push(<StyledSpell casters={Spells[currentKey]['casters']} additionalCasters={Spells[currentKey]['additionalCasters']} />);
+            spellList.push(<StyledSpell
+                casters={Spells[currentKey]['casters']}
+                additionalCasters={Spells[currentKey]['additionalCasters']}
+                level={Spells[currentKey]['level']}
+            />);
         }
 
         return spellList;
-
-        //return this.state.spells.map((spell) => <StyledSpell casters={spell.casters} additionalCasters={spell.additionalCasters} coordinates={spell.coordinates} />);
     }
 
     render() {
