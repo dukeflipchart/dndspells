7596d87b5c816aead5725740a0fa485eee8f1692
Csaba Schreinerwarning: 'C:\ProgramData/Git/config' has a dubious owner: '(unknown)'.For security reasons, it is therefore ignored.To fix this, please transfer ownership to an admininstrator <schreiner.csaba@gmail.comwarning: 'C:\ProgramData/Git/config' has a dubious owner: '(unknown)'.For security reasons, it is therefore ignored.To fix this, please transfer ownership to an admininstrator>
2019-08-20 00:34:18 +0200

    Updates

---
+++ precache-manifest.3eb7f6694b22837dc3f20a3acff53fdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eed97b651934036f021ced1b93f70d2",
+    "url": "/dndspells/index.html"
+  },
+  {
+    "revision": "415e0cc9ad781266dc96",
+    "url": "/dndspells/static/css/main.c832eda0.chunk.css"
+  },
+  {
+    "revision": "332d68dcc56c3aa56956",
+    "url": "/dndspells/static/js/2.e67ba4b3.chunk.js"
+  },
+  {
+    "revision": "415e0cc9ad781266dc96",
+    "url": "/dndspells/static/js/main.87946200.chunk.js"
+  },
+  {
+    "revision": "31b3d73b7134e406b28c",
+    "url": "/dndspells/static/js/runtime~main.14261126.js"
+  },
+  {
+    "revision": "31f30c0ffdd1c1ed3524fe3f15726728",
+    "url": "/dndspells/static/media/taurus2.31f30c0f.jpg"
+  }
+]);
